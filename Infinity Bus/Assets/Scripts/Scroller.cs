4ffe0dc1fa5a6e62afcc75cdaf4de3f233a1b55d
vid Strickland <vidkid7@gmail.com>
2015-07-12 14:47:13 -0400

    Added project to repository
    Added project to repository

---
+++ Infinity Bus/Assets/Scripts/Scroller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroller : MonoBehaviour {
+	
+	public float speed = 0;
+	public float slowDown = .05f;
+	public float increase;
+	
+	void Start() {
+
+	}
+	void Update () {
+		speed += 0.001f;
+
+		if (!Input.GetKeyDown(KeyCode.Space) && speed >= 0.001f){
+			//speed -= 0.001f;
+		}
+		
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			speed+= 0.100f;
+		}
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (increase + speed, 0f);
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Infinity-Bus
 A touch based game about driving a bus beyond the edge of reason.
+
+
+.:Game Phase 1:.
+-DONE-
+Background, desert, and highway all move with a parallax effect when you hold Spacebar, making the bus appear to move.
+
+-TO DO-
+
+
+.:Game Phase 2:.
+
+.:Game Phase 3:.
+
+.:Game Phase 4:.
+
+.:Menu:.
