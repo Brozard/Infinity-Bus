4ddc07718e2f4b6085b66e8a9134c33b531fae9a
Brozard <nickerson.brian@gmail.com>
2015-07-14 22:36:04 -0400

    Offset Update
    Added and modified floats (Time related commented out).
    Modified function Start().
    Modified function Update() and added new function FixedUpdate().
    Moved Input.GetKeyDown into new function SpeedUp().
    Added new function SlowDown().

--- Infinity Bus/Assets/Scripts/Scroller.cs
+++ Infinity Bus/Assets/Scripts/Scroller.cs
@@ -4,23 +4,50 @@ using System.Collections;
 public class Scroller : MonoBehaviour {
 	
 	public float speed = 0;
-	public float slowDown = .05f;
-	public float increase;
-	
-	void Start() {
 
-	}
-	void Update () {
-		speed += 0.001f;
-
-		if (!Input.GetKeyDown(KeyCode.Space) && speed >= 0.001f){
-			//speed -= 0.001f;
-		}
+	public float speed;
+	public float slowDown;
+	public float offset;
+	//float t;
+	//float newTime;
 		
-		if (Input.GetKeyDown(KeyCode.Space))
+	void Start()
+	{
+		rend = GetComponent<Renderer> ();
+		speed = 0f;
+		slowDown = .005f;
+	//	t = 0f;
+	}
+
+	void Update()
+	{
+	//	newTime = Time.time - t;
+		offset += speed;// * newTime;
+		rend.material.mainTextureOffset = new Vector2 (offset, 0);
+	}
+
+	void FixedUpdate ()
+	{
+		SlowDown ();
+		SpeedUp ();
+	}
+
+	void SlowDown()
+	{
+		speed -= slowDown * Time.deltaTime;
+		if (speed < 0f)
 		{
-			speed+= 0.100f;
+			speed = 0f;
 		}
-		GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (increase + speed, 0f);
+		return;
+	}
+
+	void SpeedUp()
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			speed += 0.1f * Time.deltaTime;
+		//	t = Time.time;
+		}
 	}
 }
